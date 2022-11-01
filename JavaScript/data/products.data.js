318d59f9983da5654a87bcaeadf96aa04d52b992
Ignacio Stegmann <ignaciokombat@gmail.com>
2022-11-01 20:07:55 -0300

    first commit

---
+++ JavaScript/data/products.data.js
@@ -0,0 +1,73 @@
+const products=[
+    {
+        id:1,
+        nombre: "Gorra Negra",
+        precio: 1050,
+        img:"imagenes/1.jpg",
+        category: "Zapatilla",
+    },
+    {
+        id:2,
+        nombre: "Gorra Blanca",
+        precio: 1150,
+        img:"imagenes/2.jpg",
+        category: "Zapatilla",
+    },
+    {
+        id:3,
+        nombre: "Gorra Roja",
+        precio: 950,
+        img:"imagenes/3.jpg",
+        category: "Zapatilla",
+    },
+    {
+        id:4,
+        nombre: "Vicera Plana Negra",
+        precio: 1000,
+        img:"imagenes/4",
+        category: "Zapatilla",
+    },
+    {
+        id:5,
+        nombre: "Gorra Negra",
+        precio: 1150,
+        img:"imagenes/5.jpg",
+        category: "Zapatilla",
+    }
+    ,
+    {
+        id:6,
+        nombre: "Gorra Blanca",
+        precio: 1150,
+        img:"imagenes/2.jpg",
+        category: "Zapatilla",
+    },
+    {
+        id:7,
+        nombre: "Gorra Roja",
+        precio: 950,
+        img:"imagenes/3.jpg",
+        category: "Zapatilla",
+    },
+    {
+        id:8,
+        nombre: "Visera Plana Negra",
+        precio: 1000,
+        img:"imagenes/4",
+        category: "Zapatilla",
+    },
+    {
+        id:9,
+        nombre: "Gorra Negra",
+        precio: 1150,
+        img:"imagenes/5.jpg",
+        category: "Zapatilla",
+    },
+    {
+        id:10,
+        nombre: "Gorra Blanca",
+        precio: 1150,
+        img:"imagenes/2.jpg",
+        category: "Zapatilla",
+    }
+]
